0fe9b69e8ba48a18b6601a1cfa9b9e2b3773e34d
Jose <joselmlftw@gmail.com>
2020-08-14 13:55:08 -0500

    Moviendo Logica a los componentes y alzando estado

--- src/App.js
+++ src/App.js
@@ -11,59 +11,33 @@ const containerStyle = {
 class App extends Component {
   constructor(props){
     super(props);
-    
-  this.state = {
-    newItem : '',
-    list: [{id:1, value:'Vacations'}, {id:2, value:'Work'}],
+
+    this.state = {
+      newItem : '',
+      list: [{id:1, value:'Vacations'}, {id:2, value:'Work'}],
+      }
     }
-  }
-
-  updateInput = (key, value) => {
-    this.setState({[key]: value})
-  }
-
-
-  addItem = () => {
-    const newItem = {
-      id: 1 + Math.random(),
-      value: this.state.newItem.slice()
-    };
-
-    //copy current list with ...
-    const list = [ ...this.state.list ];
-
-    //add new item to the list
-    list.push(newItem);
-
-   // update the state with the new list and clear newItem
-    this.setState({
-      list,
-      newItem : ""
-    })
-  }
 
   deleteItem = (id) =>{
-    //copy current list with ...
-    const list = [ ...this.state.list ];
-
-    //filter out item being deleted
-    const updatedList = list.filter(item => item.id !== id)
-
-    //update the list
-    this.setState({
-        list : updatedList
-    })
-}
-
-
+      //copy current list with ...
+      const list = [ ...this.state.list ];
+  
+      //filter out item being deleted
+      const updatedList = list.filter(item => item.id !== id)
+  
+      //update the list
+      this.setState({
+          list : updatedList
+      })
+  }
+  
   render(){
     return(
     <div className="App" style={containerStyle}>
     <div>
       <InputForm
-      updateInput={this.updateInput}
-      inputText = {this.state.newItem}
-      addItem = {this.addItem}
+      list={this.state.list}
+      newItem={this.state.newItem}
       />
       <List
       list={this.state.list}
@@ -75,7 +49,7 @@ class App extends Component {
     </div>  
 
   );
-}
-}
+};
+};
 
 export default App;
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react';
 
 class InputForm extends Component {
+
+  addItem = () => {
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy current list with ...
+    const list = [ ...this.state.list ];
+
+    //add new item to the list
+    list.push(newItem);
+
+   // update the state with the new list and clear newItem
+    this.setState({
+      list,
+      newItem : ""
+    })
+  }
+
   handleEnter = (e) => {
     if (e.key === 'Enter') {
       if (this.props.inputText) {
-        this.props.addItem()
+        this.addItem()
       }
     }
   }
+
+  updateInput = (key, value) => {
+    this.setState({[key]: value})
+  }
+
+
   render() {
-    const { updateInput, inputText, addItem } = this.props
+    const { list, newItem } = this.props
     return (
       <div>
         <label>Nueva Categoria</label>
         <input
         type='text'
-        value={inputText}
-        onChange={e => updateInput("newItem", e.target.value)}  
+        value={newItem}
+        onChange={e => this.updateInput("newItem", e.target.value)}  
         onKeyPress={this.handleEnter}
         >
         </input>
-        <button disabled = {!inputText.length} onClick={() => addItem()}>Agregar</button>
+        <button disabled = {!newItem.length} onClick={() => this.addItem()}>Agregar</button>
       </div>
     );
   }
